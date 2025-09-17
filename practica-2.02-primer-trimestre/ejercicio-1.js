4f11284cb6dd8280d4906f4eff8929d17364bc17
AlvaroCarrion <alvarocarrion.git@gmail.com>
2025-09-17 13:42:27 +0200

    Creamos el primer ejercicio de entrega para mañana.

---
+++ practica-2.02-primer-trimestre/ejercicio-1.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//Ejercicio 1
+export const mesDelAño = (num) => {
+    if(isNaN(num)){
+        console.log(`Lo siento, ${num}, no es un número.`);
+        return;
+    }
+    if(num < 1 || num > 12){
+        console.log('Lo siento, el número debe estár entre 1 y 12');
+        return;
+    }
+    let meses = ["Enero", "Febrero", 'Marzo','Abril','Mayo','Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    for (let i = 0; i < meses.length; i++) {
+        if(num == i){
+            //Cuando coincidan los números recogeremos el mes anterior ya que los meses empiezan desde el 1 al 12 y los arrays desde el 0.
+            //Ej -> Si nos insertan el número 1 (solicitando enero) restaremos uno para coger el elemento solicitado en la posición 0.
+            return meses[i - 1];
+        }
+    }
+};
