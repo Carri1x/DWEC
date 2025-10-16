4c1dd4681a28f1afe7c2b91c9223a1687157a987
AlvaroCarrion <alvarocarrion.git@gmail.com>
2025-10-16 19:28:34 +0200

    pra 4-02 | Terminado el primer ejercicio

--- practica-4-02/biblioteca/Ejercicio1.js
+++ practica-4-02/biblioteca/Ejercicio1.js
@@ -1,39 +1,23 @@
 "use strict";
 
-const palabrasCensuradas = [
-    'sexo',
-    'sexual',
+const sustituirContenidoBloqueado = () => {
+    let palabrasCensuradas = [
     'sexualizar',
     'sexuales',
+    'sexo',
+    'sexual', 
     'pornografía'
-];
-
-/*
-export const sustituirContenidoBloqueado = () => {
-    //Extraigo todo el contenido de body.
-    let body = document.body.innerHTML;
-    console.log(body);
-    //Compruebo que cada palabra que aparece en body no sea ninguna de las palabras censuradas previamente
-      //selecionadas en el array palabrasCensuradas.
-    for (const palabra of body) {
-        //Si la palabra en el body coincide con alguna de estas palabras censuradas la cambiamos.
-        for (const palabraCensurada of palabrasCensuradas) {
-            if(palabraCensurada == palabra){
-                palabra= '<strong class = "censurado">Contenido Bloqueado</strong>';
-            }
-        }
-    }
-    //Una vez cambiadas las palabras censuradas volvemos a inyectar el HTML en el body.
-    document.body.innerHTML = body;
-}
-*/
-
-export const sustituirContenidoBloqueado = () => {
-    let body = document.body.innerHTML;
+    ];
+    let cuerpo = document.body.innerHTML;
+    let cuerpoNuevo = document.body;
     for (const palabraCensurada of palabrasCensuradas) {
-        body.replaceAll(palabraCensurada, '<strong class = "censurado">Contenido Bloqueado</strong>');
+        cuerpo = cuerpo.replaceAll(
+            palabraCensurada,
+            '<strong class = "censurado">Contenido Bloqueado</strong>'
+        );
     }
-    document.body.innerHTML = body;
+    cuerpoNuevo.innerHTML = cuerpo;
 }
 
+export {sustituirContenidoBloqueado};
 
