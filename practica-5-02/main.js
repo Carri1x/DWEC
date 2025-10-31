e0ee647ebb265c20e1150f8abe5a99ec5bc3031d
AlvaroCarrion <alvarocarrion.git@gmail.com>
2025-10-31 16:31:44 +0100

    pract 5-02 | Falta por mejorar ACORDEÓN

--- practica-5-02/main.js
+++ practica-5-02/main.js
@@ -39,6 +39,41 @@ window.onload = () => {
 
     //----------------------------------------------------------------------- Ejerciciof 2: Pestañas -----------------------------------------------------------------------
 
+    let tabs = document.getElementsByClassName('tab')
+    for (let i = 0; i < tabs.length; i++) {
+
+        tabs[i].addEventListener("click", (event) => {
+            let elemento = event.target;
+            console.log(elemento);
+            let posicion = 0;
+            let divContenedor = elemento.parentElement; //Cogemos el padre del elemento que ha hecho saltar el evento.
+            let hijosDivContenedor = divContenedor.children; //Cogemos los hijos del contenedor padre.
+            for (let i = 0; i < hijosDivContenedor.length; i++) {
+                if(hijosDivContenedor[i] === elemento){
+                    posicion = i; //Ahora que sabemos cual es el hijo que ha hecho saltar el evento. Igualamos la posición.
+                }
+            }
+            if(!divContenedor.nextElementSibling) return; //Si no tiene hermano siguiente es porque es el contenedor erróneo.
+            let divHermano = divContenedor.nextElementSibling; //Cogemos el contenedor hermano siguiente.
+            let hijosDelDivHermano = divHermano.children;
+            let contenidoObjetivo = divHermano.children[posicion]; //Cogemos el CONTENIDO PESTAÑA que queremos mostrar.
+
+            if(!contenidoObjetivo.classList.contains('oculto'))return; //Si el contenido que queremos manipular ya sale en la pantalla no lo ocultamos lo seguimos manteniendo.
+
+            for (let i = 0; i < hijosDelDivHermano.length; i++) { 
+                if(hijosDelDivHermano[i] !== contenidoObjetivo){ //Si los elementos son diferentes al contendio que debe ser objetivo.
+                    if(!hijosDelDivHermano[i].classList.contains('oculto')){ //Si no contiene la clase oculto se la aplica.
+                        hijosDelDivHermano[i].classList.add('oculto'); // Aplica la clase oculto.
+                    } 
+                    
+                }
+
+            }
+            contenidoObjetivo.classList.toggle('oculto'); //Cambiamos la clase.
+        });
+        //Educada y cordialmente, ¡¡¡AAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHH VA BIEN JDR, DE P*** MADRE JDR!!!. :DD Perdona por la locura momentánea.
+
+    }
     
 
 
