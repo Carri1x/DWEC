07d7a237e2875addbb8f16aab33b34e633282c94
AlvaroCarrion <alvarocarrion.git@gmail.com>
2025-10-31 07:45:10 +0100

    pract 5-02 || confundidio con el ejercicio 2

--- practica-5-02/main.js
+++ practica-5-02/main.js
@@ -9,14 +9,38 @@ import { hacerHermanoVisible } from "./biblioteca/Ejercicio1.js";
 
 window.onload = () => {
     
+    //----------------------------------------------------------------------- Ejerciciof 1: Acordeón -----------------------------------------------------------------------
     let acordeon = document.getElementById('acordeon'); //Extraigo la referencia del DIV acordeon.
     
     acordeon.addEventListener('click', (event) => {
         let elemento = event.target;
+        if(elemento === 'div#acordeon.oculto'){
+            return;
+        }
+        if (elemento.nextElementSibling.classList.contains('oculto')){ //He intentado hacer una transición...
+            let contador = 0;
+            let transicion = setInterval(() => {
+                elemento.style.marginBottom = `${contador++}px`;
+                if(contador === 21){
+                    clearInterval(transicion);
+                }
+            }, 10);
+            elemento.style.marginBottom = '0px';
+        }
+        elemento.style.marginBottom = '0px';
         if(elemento.classList.contains('visible')){ //Si el elemento tienen la clase visible quitamos la clase oculto del siguiente elemento.
             hacerHermanoVisible(elemento); 
             return; //Sin return se auto quitaría el mismo objeto que ha sido clicado.
         }
         elemento.classList.toggle('oculto'); // Si no tiene la clase 'visible' se añade la clase oculto. 
+        
     });
+
+
+    //----------------------------------------------------------------------- Ejerciciof 2: Pestañas -----------------------------------------------------------------------
+
+    
+
+
+
 } //FIN DEL WINDOW.ONLOAD!!
