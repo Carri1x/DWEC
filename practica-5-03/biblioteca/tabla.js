c9bb074239eba1a7199d9e79129515aeda31b0a2
AlvaroCarrion <alvarocarrion.git@gmail.com>
2025-11-02 20:11:00 +0100

    puestos los comentarios

--- practica-5-03/biblioteca/tabla.js
+++ practica-5-03/biblioteca/tabla.js
@@ -1,17 +1,17 @@
 "use strict";
 
 export const tablaConstructor = (alto, ancho) => {
-    let tabla = document.createElement('table');
+    let tabla = document.createElement('table'); //Creamos la tabla.
     for (let i = 0; i < alto; i++) {
-        let fila = document.createElement('tr');
-        tabla.appendChild(fila);
+        let fila = document.createElement('tr'); //Creamos una fila.
+        tabla.appendChild(fila); //Añadimos la fila a la tabla.
         for (let j = 0; j < ancho; j++) {
-            let celda = document.createElement('td');
+            let celda = document.createElement('td'); //Creamos una celda.
             celda.style.backgroundColor = '#ffffff';
             celda.style.width = '10px';
             celda.style.height = '10px';
             celda.style.border = '1px solid white';
-            fila.appendChild(celda);
+            fila.appendChild(celda); //Añadimos la celda a la fila.
         }
     }
     tabla.style.border = '1px solid black';
@@ -22,14 +22,14 @@ export const tablaConstructor = (alto, ancho) => {
 export const cambiarTamañoCeldas = (tabla, tamaño) => {
     if(!tabla || tabla.tagName !== 'TABLE') return; //En caso de que sea undefined o no sea una tabla no hacemos nada.
 
-    let filas = tabla.querySelectorAll('tr');
+    let filas = tabla.querySelectorAll('tr'); //Seleccionamos todas las filas de la tabla.
 
     for (let i = 0; i < filas.length; i++) {
-        let celdas = filas[i].querySelectorAll('td');
+        let celdas = filas[i].querySelectorAll('td'); //Seleccionamos todas las celdas de la fila actual.
 
         for (let j = 0; j < celdas.length; j++) {
-            celdas[j].style.width = `${tamaño}px`;
-            celdas[j].style.height = `${tamaño}px`;
+            celdas[j].style.width = `${tamaño}px`; //Cambiamos el tamaño de la celda.
+            celdas[j].style.height = `${tamaño}px`; //Cambiamos el tamaño de la celda.
         }
     }
 }
--- practica-5-03/main.js
+++ practica-5-03/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { cambiarTamañoCeldas, tablaConstructor } from "./biblioteca/tabla.js";
+import { tablaConstructor } from "./biblioteca/tabla.js";
 
 window.onload = () => {
     let main = document.getElementsByClassName('main')[0]; //Solo vamos a tener un elemento main.
@@ -10,48 +10,39 @@ window.onload = () => {
     let colorSelec = '#FFFFFF';
     let pintando = false;
 
+    //Eventos para la selección de color y el pintado del lienzo.
     main.addEventListener('click', (evento) => {
         let elemento = evento.target;
-        if (elemento.classList.contains('color')) {
-            colorSelec = elemento.style.backgroundColor;
+        if (elemento.classList.contains('color')) { //Si el elemento tiene la clase color.
+            colorSelec = elemento.style.backgroundColor; //Cambiamos el color seleccionado por el del div clicado.
         }
-        if (elemento.tagName === 'BUTTON'){
-            window.location.reload();
+        if (elemento.tagName === 'BUTTON'){ //Si clicamos el botón de borrar.
+            window.location.reload(); //Recargamos la página para borrar el lienzo.
         }
     });
 
     main.addEventListener('mousedown',(evento) => {
-        if(evento.target.tagName === 'TD'){
-            pintando = true;
+        if(evento.target.tagName === 'TD'){ //Si clicamos sobre una celda.
+            pintando = true; //Activamos el pintado.
+            evento.target.style.backgroundColor = colorSelec; //Cambiamos el color de la celda clicada.
         }
     });
 
-    main.addEventListener('mousemove', (evento) => {
+    main.addEventListener('mousemove', (evento) => { //Mientras movemos el ratón.
         let elemento = evento.target;
         
-        if(pintando && elemento.tagName === 'TD'){
-            console.log(colorSelec)
-            elemento.style.backgroundColor = colorSelec;
+        if(pintando && elemento.tagName === 'TD'){ //Si estamos pintando y el elemento es una celda.
+            elemento.style.backgroundColor = colorSelec; //Cambiamos el color de las celdas por donde pasemos.
         }
     });
 
-    main.addEventListener('mouseup',(evento) => {
-        if(evento.target.tagName === 'TD'){
-            pintando = false;
+    main.addEventListener('mouseup',(evento) => { //Cuando soltamos el botón del ratón.
+        if(evento.target.tagName === 'TD'){ //Si el elemento es una celda.
+            pintando = false; //Desactivamos el pintado.
         }
     });
-
-
-
-
-
-
-
-
-
 
 } //¡¡FIN DEL WINDOW.ONLOAD!!
-
 
 
 const crearLayoutColoresYLienzo = (main) => {
